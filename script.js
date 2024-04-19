0bd3a07d97b08ed221523a54cb26c8dfa41f5ce2
Joao-Victor-Zanatto <165964840+Joao-Victor-Zanatto@users.noreply.github.com>
2024-04-19 00:52:27 -0300

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const titulo = document.getElementsByName('hello')[0];
-const buttonHello = document.getElementById('buttonHello');
-const paragrafo = document.querySelector('.parag');
+// Função para fazer uma mensagem de 'Olá Mundo'
+function add_ola_mundo() {
+    document.getElementById("texto1").innerText = "Olá Mundo";
+}
 
-buttonHello.addEventListener('click', function() {
-    alert('Hello World');
-    n1 = Number(prompt('Digite um número:'));
-    n2 = Number(prompt('Digite um número:'));
-    soma = n1 + n2;
+// Função para fazer uma saída de soma de 2 inteiros
+function gerar_calc() {
+    var num1 = parseInt(document.getElementById("num1_att2").value);
+    var num2 = parseInt(document.getElementById("num2_att2").value);
+    var resultado = num1 + num2;
+    document.getElementById("res_att2").innerText = "O resultado é: " + resultado;
+}
 
-    titulo.innerHTML = `A soma de ${n1} + ${n2} é igual a ${soma}`;
-    paragrafo.style.color = 'red';
-});
+// Função para alterar a propriedade de um elemento por id
+function alt_id() {
+    document.getElementById("alterarPorId").style.color = "red";
+    document.getElementById("alterarPorId").innerText = "Propriedade alterada por id!";
+}
+
+// Função para alterar a propriedade de um elemento por name
+function alt_name() {
+    var elements = document.getElementsByName("alterarPorName");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontWeight = "bold";
+        elements[i].innerHTML = "Propriedade alterada por name!";
+    }
+}
+
+// Função para alterar a propriedade de um elemento por class
+function alt_class() {
+    var elements = document.getElementsByClassName("alterarPorClass");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontStyle = "italic";
+        elements[i].innerHTML = "Propriedade alterada por class!";
+    }
+}
